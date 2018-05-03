ba4c41edb934fb5ea6e99934181b27a93e09984d
Philippe Gervaise <github@malvese.org>
2018-05-03 19:13:23 +0200

    Create fr.php

---
+++ languages/fr.php
@@ -0,0 +1,7 @@
+
+<?php
+l::set([
+    'broken-links.brokenLinks' => 'Liens cassés',
+    'broken-links.ok' => 'Tous les liens internes semblent fonctionner.',
+    'broken-links.check' => 'Vérifier maintenant'
+]);
